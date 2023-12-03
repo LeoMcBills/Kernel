d7e4c16debb22109d56732eb24a31c094ed68ad1
LeoMcBills <leokinyera9@gmail.com>
2023-12-03 13:02:26 +0300

    shell struct

---
+++ leoSh.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+
+int main(int argc, char **argv)
+{
+    // Load config files, if any of them.
+
+    // Run command loop.
+    //lsh_loop();
+
+    // Perform any shutdown/cleanup.
+
+    return EXIT_SUCCESS;
+}
